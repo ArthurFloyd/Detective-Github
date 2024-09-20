85f7fb63c52bdb8a77da93033b0104adf23f7ce5
arthur voryshev <what_to_write@mail.ru>
2024-09-20 22:47:52 +1000

    create coment

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,7 +15,7 @@ const NavBar = () => {
   useEffect(() => {
     inputRef.current.focus();
   }, []);
-
+  // TODO: fix
   const { data } = useGetReposQuery(nameRepos);
   console.log(data)
   return (
